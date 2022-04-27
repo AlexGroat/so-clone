94990b70fcad3670a2da8dbcca428f9c764bbb75
Alex Groat <alex.groat.95@gmail.com>
2022-04-27 15:26:55 +0930

    designing question schema, define user and question model relationships

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // mutator
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        // slug will be set to the title in slug format
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
